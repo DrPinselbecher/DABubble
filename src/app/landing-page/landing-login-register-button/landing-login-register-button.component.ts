be8cc0d2cf58bbc7f25bf9e871122b62c8913079
René Porzelt <rene.tube992@gmail.com>
2024-12-09 12:09:08 +0100

    fix a bug

--- src/app/landing-page/landing-login-register-button/landing-login-register-button.component.ts
+++ src/app/landing-page/landing-login-register-button/landing-login-register-button.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-login-register-button',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive,],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-login-register-button.component.html',
   styleUrl: './landing-login-register-button.component.scss'
 })
